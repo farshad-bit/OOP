6c13765eae93672f52718a01dc5dde57d75c8592
farshad-bit <128085137+farshad-bit@users.noreply.github.com>
2023-05-04 20:09:07 +0200

    Update Rectangle.php

--- aufgabe4/Rectangle.php
+++ aufgabe4/Rectangle.php
@@ -1,1 +1,20 @@
+<?php  
 
+require 'Shape.php';
+
+class Rectangle extends Shape {
+     public function getArea():float{
+        return $this->width * $this->height;
+    }
+    public $width;
+    public $height;
+
+    public function __construct($width, $height){
+        $this->width = $width;
+        $this->height = $height;
+    }
+} 
+$rectanlage = new Rectangle(10, 5);
+echo "Die Rectanlage ist " . $rectanlage->getArea() . "<br>";
+
+?>
